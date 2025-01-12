09208cd484fc0d3a5c7240fd462dce7e3a0b8a52
Yufanggg <wangalisa2017@gmail.com>
2025-01-12 17:31:18 +0100

    update etl file

--- ETL.py
+++ ETL.py
@@ -76,11 +76,6 @@ class ETL:
             self.null_non_null_table.to_sql("UScovid_tracking_null_non_null", sql_engine, if_exists = "replace")
             print("STEP 3: Done LOADING data into database. \n...")
 
-            # save data frame to SQL
-            self.df.to_sql('UScovid_tracking', sql_engine, if_exists='replace')
-            self.null_non_null_table.to_sql("UScovid_tracking_null_non_null", sql_engine, if_exists = "replace")
-
-            print("STEP 3: Done LOADING data into database. \n...")
         except Exception as e:
             print("Data load error: " + str(e))
 
